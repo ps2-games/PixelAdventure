2b49871241e217f3948905485b739f4510266c43
Gibran Khalil <gibrankhalil150904@gmail.com>
2025-04-24 17:00:37 -0300

    FEAT: adição de frutas do level2

---
+++ src/core/levels/level2/constants/fruits.js
@@ -0,0 +1,21 @@
+import FruitUtils from "../../../../models/entities/Fruits/utils/index.js";
+import { TILE_SIZE } from "../../../Scene/constants/index.js";
+
+const fruitsLevel2 = [
+    ...FruitUtils.drawVerticalLine(76, 330, 5, "Strawberry", TILE_SIZE * 3.8),
+    ...FruitUtils.drawVerticalLine(48, 304, 5, "Strawberry", TILE_SIZE * 3.8),
+    ...FruitUtils.drawHorizontalLine(250, 170, 6, "Strawberry", TILE_SIZE * 1.5),
+    ...FruitUtils.drawHorizontalLine(440, 126, 2, "Bananas", TILE_SIZE * 4),
+    ...FruitUtils.drawVerticalLine(190, 338, 8, "Bananas", TILE_SIZE * 2.25),
+    ...FruitUtils.drawTriangle(464, 330, 3, "Bananas", TILE_SIZE * 1.5),
+    ...FruitUtils.drawHorizontalLine(298, 340, 2, "Strawberry", TILE_SIZE * 2.5),
+    ...FruitUtils.drawHorizontalLine(278, 362, 3, "Strawberry", TILE_SIZE * 2.5),
+    {
+        x: 560,
+        y: 236,
+        type: "Strawberry"
+    }
+]
+
+export default fruitsLevel2;
+
--- src/core/levels/level2/main.js
+++ src/core/levels/level2/main.js
@@ -1,14 +1,15 @@
 import backgroundConfigLevel2 from "./constants/background.js";
+import fruitsLevel2 from "./constants/fruits.js";
 import mapLevel2 from "./constants/map.js";
 
 const level2Config = {
     backgroundConfig: backgroundConfigLevel2,
     tileMapConfig: mapLevel2,
-    fruits: null,
+    fruits: fruitsLevel2,
     traps: null,
     initialPlayerPosition: {
         x: 64,
-        y: 336
+        y: 367
     }
 }
 
