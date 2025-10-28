dcac797fddbdeca2ea38e00cda0c546d55d6348c
GibranKhalil <gibrankhalil150904@gmail.com>
2025-10-28 10:53:53 -0300

    refactor: tileMap

--- src/levels/level1/constants/map.js
+++ src/levels/level1/constants/map.js
@@ -1,6 +1,4 @@
 import { TILE_SIZE, TILE_TYPES } from "../../../constants.js";
-import ground from "./ground.js";
-import platforms from "./platforms.js";
 
 const leftWood = Array.from({ length: 15 }, (_, i) => ({
   tileColumn: 2,
@@ -61,8 +59,241 @@ const fixedTiles = [
   { tileColumn: 12, tileRow: 1, tileX: 576, tileY: 328, type: TILE_TYPES.WALL } // rightBottomSmallBox
 ];
 
+const groundQuarterFloor = [
+  {
+    tileColumn: 6,
+    tileRow: 0,
+    tileX: 224,
+    tileY: 280,
+    type: TILE_TYPES.GROUND
+  },
+  ...Array.from({ length: 8 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 0,
+    tileX: 240 + i * TILE_SIZE,
+    tileY: 280,
+    type: TILE_TYPES.GROUND
+  })),
+  {
+    tileColumn: 8,
+    tileRow: 0,
+    tileX: 368,
+    tileY: 280,
+    type: TILE_TYPES.GROUND
+  },
+]
+
+const groundThirdFloor = [
+  {
+    tileColumn: 6,
+    tileRow: 1,
+    tileX: 224,
+    tileY: 296,
+    type: TILE_TYPES.GROUND
+  },
+  ...Array.from({ length: 8 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 1,
+    tileX: 240 + i * TILE_SIZE,
+    tileY: 296,
+    type: TILE_TYPES.NON_COLLIDABLE
+  })),
+  {
+    tileColumn: 8,
+    tileRow: 1,
+    tileX: 368,
+    tileY: 296,
+    type: TILE_TYPES.GROUND
+  },
+  {
+    tileColumn: 6,
+    tileRow: 0,
+    tileX: 512,
+    tileY: 296,
+    type: TILE_TYPES.GROUND
+  },
+  ...Array.from({ length: 3 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 0,
+    tileX: 528 + i * TILE_SIZE,
+    tileY: 296,
+    type: TILE_TYPES.GROUND
+  })),
+  {
+    tileColumn: 8,
+    tileRow: 0,
+    tileX: 576,
+    tileY: 296,
+    type: TILE_TYPES.NON_COLLIDABLE
+  },
+]
+
+const groundSecondFloor = [
+  {
+    tileColumn: 6,
+    tileRow: 0,
+    tileX: 208,
+    tileY: 312,
+    type: TILE_TYPES.GROUND
+  },
+  {
+    tileColumn: 7,
+    tileRow: 0,
+    tileX: 224,
+    tileY: 312,
+    type: TILE_TYPES.NON_COLLIDABLE
+  },
+  ...Array.from({ length: 8 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 1,
+    tileX: 240 + i * TILE_SIZE,
+    tileY: 312,
+    type: TILE_TYPES.NON_COLLIDABLE
+  })),
+  ...Array.from({ length: 2 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 0,
+    tileX: 368 + i * TILE_SIZE,
+    tileY: 312,
+    type: i === 0 ? TILE_TYPES.GROUND : TILE_TYPES.NON_COLLIDABLE
+  })),
+  {
+    tileColumn: 8,
+    tileRow: 0,
+    tileX: 400,
+    tileY: 312,
+    type: TILE_TYPES.GROUND
+  },
+  {
+    tileColumn: 6,
+    tileRow: 1,
+    tileX: 512,
+    tileY: 312,
+    type: TILE_TYPES.GROUND
+  },
+  ...Array.from({ length: 3 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 1,
+    tileX: 528 + i * TILE_SIZE,
+    tileY: 312,
+    type: TILE_TYPES.NON_COLLIDABLE
+  })),
+  {
+    tileColumn: 8,
+    tileRow: 1,
+    tileX: 576,
+    tileY: 312,
+    type: TILE_TYPES.NON_COLLIDABLE
+  },
+]
+
+const groundFirstFloor = [
+  {
+    tileColumn: 6,
+    tileRow: 0,
+    tileX: 176,
+    tileY: 328,
+    type: TILE_TYPES.GROUND
+  },
+  ...Array.from({ length: 4 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 0,
+    tileX: 192 + i * TILE_SIZE,
+    tileY: 328,
+    type: i === 0 ? TILE_TYPES.GROUND : TILE_TYPES.NON_COLLIDABLE
+  })),
+  ...Array.from({ length: 9 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 1,
+    tileX: 256 + i * TILE_SIZE,
+    tileY: 328,
+    type: TILE_TYPES.NON_COLLIDABLE
+  })),
+  ...Array.from({ length: 9 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 0,
+    tileX: 400 + i * TILE_SIZE,
+    tileY: 328,
+    type: i === 0 || i > 6 ? TILE_TYPES.NON_COLLIDABLE : TILE_TYPES.GROUND
+  })),
+  ...Array.from({ length: 2 }, (_, i) => ({
+    tileColumn: 7,
+    tileRow: 1,
+    tileX: 544 + i * TILE_SIZE,
+    tileY: 328,
+    type: TILE_TYPES.NON_COLLIDABLE
+  })),
+]
+
+const leftSideStickPlatform = Array.from({ length: 4 }, (_, i) => ({
+  tileColumn: i === 3 ? 19 : 18,
+  tileRow: 1,
+  tileX: 64 + i * TILE_SIZE,
+  tileY: 296,
+  type: TILE_TYPES.PLATFORM
+}))
+
+const leftSideBoxPlatform = [
+  {
+    tileColumn: 12,
+    tileRow: 1,
+    tileX: 160,
+    tileY: 216,
+    type: TILE_TYPES.GROUND
+  },
+  ...Array.from({ length: 3 }, (_, i) => ({
+    tileColumn: i === 0 ? 12 : i === 2 ? 14 : 13,
+    tileRow: 0,
+    tileX: 176 + i * TILE_SIZE,
+    tileY: 216,
+    type: TILE_TYPES.GROUND
+  }))
+]
+
+const rightSideCrossBoxPlatform = [
+  ...Array.from({ length: 2 }, (_, i) => ({
+    tileColumn: 12,
+    tileRow: 1,
+    tileX: 336 + i * TILE_SIZE,
+    tileY: 184,
+    type: TILE_TYPES.GROUND
+  })),
+  {
+    tileColumn: 15,
+    tileRow: 0,
+    tileX: 368,
+    tileY: 184,
+    type: TILE_TYPES.GROUND
+  },
+  {
+    tileColumn: 15,
+    tileRow: 2,
+    tileX: 368,
+    tileY: 200,
+    type: TILE_TYPES.GROUND
+  },
+  {
+    tileColumn: 12,
+    tileRow: 1,
+    tileX: 384,
+    tileY: 184,
+    type: TILE_TYPES.GROUND
+  }
+]
+
+const rightSideStickPlatform = Array.from({ length: 4 }, (_, i) => ({
+  tileColumn: i === 3 ? 17 : 18,
+  tileRow: 1,
+  tileX: 560 - i * TILE_SIZE,
+  tileY: 248,
+  type: TILE_TYPES.PLATFORM
+}))
+
 const mapLevel1 = [
-  ...ground,
+  ...groundQuarterFloor,
+  ...groundThirdFloor,
+  ...groundSecondFloor,
+  ...groundFirstFloor,
   ...leftWood,
   ...topStone,
   ...bottomStone,
@@ -70,7 +301,10 @@ const mapLevel1 = [
   ...rightWoodBottom,
   ...fixedTiles,
   ...leftTopBigBox,
-  ...platforms
+  ...leftSideStickPlatform,
+  ...leftSideBoxPlatform,
+  ...rightSideCrossBoxPlatform,
+  ...rightSideStickPlatform
 ];
 
 export default mapLevel1;
